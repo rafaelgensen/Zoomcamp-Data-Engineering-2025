7fed1cc0b1d2758c2a8c1deba3e94a01de99486d
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-06-24 01:54:35 -0300

    ajust logic and system integration, bash -> executable py

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+
+import sys
+
+import pandas as pd # type: ignore
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+print(f'job finished successfully for day = {day}')
+
+## gitbash
+
+# docker build -t test .
+# docker run -it test
